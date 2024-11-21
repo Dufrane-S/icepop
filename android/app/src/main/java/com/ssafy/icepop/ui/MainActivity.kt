3b8efaea5e4847b7de788be23d0aa5726f28769f
gretea5 <gretea4489@gmail.com>
2024-11-21 10:33:28 +0900

    Add : LoginFragment xml 완성

--- android/app/src/main/java/com/ssafy/icepop/ui/MainActivity.kt
+++ android/app/src/main/java/com/ssafy/icepop/ui/MainActivity.kt
@@ -3,9 +3,11 @@ package com.ssafy.icepop.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.ssafy.icepop.R
 import com.ssafy.icepop.data.model.dto.User
 import com.ssafy.icepop.data.remote.RetrofitUtil
 import com.ssafy.icepop.databinding.ActivityMainBinding
+import com.ssafy.icepop.ui.auth.LoginFragment
 import com.ssafy.smartstore_jetpack.base.BaseActivity
 import kotlinx.coroutines.launch
 
@@ -14,14 +16,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        lifecycleScope.launch {
-            kotlin.runCatching {
-                RetrofitUtil.userService.login(User("test", "test"))
-            }.onSuccess {
-                Log.d(TAG, "onCreate: ${it}")
-            }.onFailure {
-                Log.d(TAG, "실패:")
-            }
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_layout, LoginFragment())
+            .commit()
     }
 }
--- android/app/src/main/java/com/ssafy/icepop/ui/auth/LoginFragment.kt
+++ android/app/src/main/java/com/ssafy/icepop/ui/auth/LoginFragment.kt
@@ -1,10 +1,7 @@
 package com.ssafy.icepop.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.ssafy.icepop.R
 import com.ssafy.icepop.databinding.FragmentLoginBinding
 import com.ssafy.smartstore_jetpack.base.BaseFragment
@@ -14,23 +11,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
     FragmentLoginBinding::bind,
     R.layout.fragment_login
 ) {
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
