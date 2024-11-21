061e11fd06274e2ab664da0df979fae6a0bbf457
gretea5 <gretea4489@gmail.com>
2024-11-21 23:07:16 +0900

    Add : fragment_ice_cream_list.xml에 fab, tablayout 추가

--- android/app/src/main/java/com/ssafy/icepop/ui/list/IceCreamListFragment.kt
+++ android/app/src/main/java/com/ssafy/icepop/ui/list/IceCreamListFragment.kt
@@ -1,5 +1,7 @@
 package com.ssafy.icepop.ui.list
 
+import android.os.Bundle
+import android.view.View
 import com.ssafy.icepop.R
 import com.ssafy.icepop.databinding.FragmentIceCreamListBinding
 import com.ssafy.smartstore_jetpack.base.BaseFragment
@@ -8,4 +10,7 @@ class IceCreamListFragment : BaseFragment<FragmentIceCreamListBinding> (
     FragmentIceCreamListBinding::bind,
     R.layout.fragment_ice_cream_list
 ){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
