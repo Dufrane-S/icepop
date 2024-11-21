c71ac174e6c60afff6e71d915685927cb50d6de8
gretea5 <gretea4489@gmail.com>
2024-11-22 01:00:55 +0900

    Add : tablayout 필터링 icon, Visibiltiy gond, visible 처리

--- android/app/src/main/java/com/ssafy/icepop/ui/list/IceCreamListFragment.kt
+++ android/app/src/main/java/com/ssafy/icepop/ui/list/IceCreamListFragment.kt
@@ -2,6 +2,8 @@ package com.ssafy.icepop.ui.list
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TableLayout
+import com.google.android.material.tabs.TabLayout
 import com.ssafy.icepop.R
 import com.ssafy.icepop.databinding.FragmentIceCreamListBinding
 import com.ssafy.smartstore_jetpack.base.BaseFragment
@@ -12,5 +14,34 @@ class IceCreamListFragment : BaseFragment<FragmentIceCreamListBinding> (
 ){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.tabLayout.addOnTabSelectedListener (object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                // 탭이 선택될 때 실행할 코드
+                when (tab.position) {
+                    TYPE -> {
+                        binding.optionArea.visibility = View.VISIBLE
+                    }
+                    POPULARITY -> {
+                        binding.optionArea.visibility = View.VISIBLE
+                    }
+                    AI_RECOMMEND -> {
+                        binding.optionArea.visibility = View.GONE
+                    }
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+    companion object {
+        const val TYPE = 0
+        const val POPULARITY = 1
+        const val AI_RECOMMEND = 2
     }
 }
