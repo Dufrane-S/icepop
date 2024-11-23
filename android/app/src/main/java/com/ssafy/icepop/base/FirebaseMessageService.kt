37fe121a5046198f5fdc7b158b8e38019d3e7f14
gretea5 <gretea4489@gmail.com>
2024-11-23 17:41:20 +0900

    Fix : FCM 알림 Data 메시지 수신으로 로직 변경

--- android/app/src/main/java/com/ssafy/icepop/base/FirebaseMessageService.kt
+++ android/app/src/main/java/com/ssafy/icepop/base/FirebaseMessageService.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.ssafy.icepop.R
 import com.ssafy.icepop.ui.MainActivity
 import com.ssafy.icepop.ui.auth.AuthActivity
 
@@ -22,29 +23,38 @@ class FirebaseMessageService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "onMessageReceived: ${remoteMessage.notification}")
 
-        remoteMessage.notification?.let { message ->
-            Log.d(TAG, "onMessageReceived: $message")
-            val messageTitle = message.title
-            val messageContent = message.body
+        var messageTitle = ""
+        var messageContent = ""
 
-            val mainIntent = Intent(this, MainActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            }
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "onMessageReceived: not null")
+            messageTitle = remoteMessage.notification!!.title.toString()
+            messageContent = remoteMessage.notification!!.body.toString()
+        }
+        else {
+            messageTitle = remoteMessage.data.get("myTitle").toString()
+            messageContent = remoteMessage.data.get("orderId").toString()
 
-            //mainIntent를 인자로 보내줘
-            //클릭이 될지 안될지 모르는 인텐트 pendingintent
-            val mainPendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_IMMUTABLE)
+            Log.d(TAG, "onMessageReceived: $messageTitle $messageContent")
+        }
 
-            val builder1 = NotificationCompat.Builder(this, AuthActivity.channel_id)
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle(messageTitle)
-                .setContentText(messageContent)
-                .setAutoCancel(true)
-                .setContentIntent(mainPendingIntent)
+        val mainIntent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+
+        //mainIntent를 인자로 보내줘
+        //클릭이 될지 안될지 모르는 인텐트 pendingintent
+        val mainPendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder1 = NotificationCompat.Builder(this, AuthActivity.channel_id)
+            .setSmallIcon(R.drawable.app_logo)
+            .setContentTitle(messageTitle)
+            .setContentText(messageContent)
+            .setAutoCancel(true)
+            .setContentIntent(mainPendingIntent)
 //                .setPriority(NotificationCompat.PRIORITY_HIGH)      // head up message
 
-            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(101, builder1.build())
-        }
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(101, builder1.build())
     }
 }
