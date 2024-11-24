4b4c271dc6271161396e41bda25af6df1b80b4d3
gretea5 <gretea4489@gmail.com>
2024-11-24 19:29:26 +0900

    Add : 모든 주문 내역 보여주기

--- android/app/src/main/java/com/ssafy/icepop/ui/order/OrderListAdapter.kt
+++ android/app/src/main/java/com/ssafy/icepop/ui/order/OrderListAdapter.kt
@@ -3,17 +3,52 @@ package com.ssafy.icepop.ui.order
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.ssafy.icepop.data.model.dto.IceCreamOrder
 import com.ssafy.icepop.databinding.ItemOrderBinding
+import com.ssafy.smartstore_jetpack.base.ApplicationClass.Companion.ICE_CREAM_IMAGE_BASE_URL
+import com.ssafy.smartstore_jetpack.util.CommonUtils
 
 class OrderListAdapter (
     var iceCreamOrderList : List<IceCreamOrder>,
-    private val onItemClickListener : (Unit) -> Unit
+    private val onItemClickListener : (IceCreamOrder) -> Unit
 ) : RecyclerView.Adapter<OrderListAdapter.OrderListViewHolder>() {
     inner class OrderListViewHolder(private val binding: ItemOrderBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(iceCreamOrder: IceCreamOrder) {}
+        fun bind(iceCreamOrder: IceCreamOrder) {
+            val primaryName = if (iceCreamOrder.details.isNotEmpty()) {
+                iceCreamOrder.details[0].iceName
+            }
+            else {
+                "아이템 없음"
+            }
+
+            val primaryImage = if (iceCreamOrder.details.isNotEmpty()) {
+                iceCreamOrder.details[0].img
+            }
+            else {
+                "이미지 없음"
+            }
+
+            val additionalCount = iceCreamOrder.details.size - 1
+
+            val displayText = if (additionalCount > 0) {
+                "$primaryName 외 ${additionalCount}개"
+            } else {
+                primaryName
+            }
+
+            binding.orderName.text = displayText
+            binding.orderPrice.text = CommonUtils.makeComma(iceCreamOrder.resultSum)
+            binding.orderDateTimeTv.text = CommonUtils.formatLongToDateTime(iceCreamOrder.date)
+
+            val imageUrl = ICE_CREAM_IMAGE_BASE_URL + primaryImage
+
+            Glide.with(binding.root).load(imageUrl).into(binding.orderImage)
+
+            binding.root.setOnClickListener { onItemClickListener(iceCreamOrder) }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderListViewHolder {
--- android/app/src/main/java/com/ssafy/icepop/ui/order/OrderListFragment.kt
+++ android/app/src/main/java/com/ssafy/icepop/ui/order/OrderListFragment.kt
@@ -2,17 +2,28 @@ package com.ssafy.icepop.ui.order
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.icepop.R
+import com.ssafy.icepop.data.model.dto.IceCreamOrder
+import com.ssafy.icepop.data.model.dto.request.OrderRequest
+import com.ssafy.icepop.data.remote.RetrofitUtil
 import com.ssafy.icepop.databinding.FragmentOrderListBinding
 import com.ssafy.icepop.ui.MainActivity
+import com.ssafy.smartstore_jetpack.base.ApplicationClass
 import com.ssafy.smartstore_jetpack.base.BaseFragment
+import com.ssafy.smartstore_jetpack.util.CommonUtils
+import kotlinx.coroutines.launch
 
-
+private const val TAG = "OrderListFragment_ssafy"
 class OrderListFragment : BaseFragment<FragmentOrderListBinding>(
     FragmentOrderListBinding::bind,
     R.layout.fragment_order_list
 ) {
     private lateinit var mainActivity: MainActivity
+    private lateinit var orderListAdapter: OrderListAdapter
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -23,5 +34,45 @@ class OrderListFragment : BaseFragment<FragmentOrderListBinding>(
         super.onCreate(savedInstanceState)
 
         mainActivity.hideBottomNav(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+        getAllOrder()
+    }
+
+    private fun initAdapter() {
+        orderListAdapter = OrderListAdapter(mutableListOf()) {
+
+        }
+
+        binding.orderListRv.apply {
+            adapter = orderListAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+
+        CommonUtils.setVerticalDivider(binding.orderListRv.context, binding.orderListRv)
+    }
+
+    private fun getAllOrder() {
+        val email = ApplicationClass.sharedPreferencesUtil.getUser().email
+
+        lifecycleScope.launch {
+            runCatching {
+                RetrofitUtil.orderService.getOrderList(OrderRequest(email = email, recent = true))
+            }.onSuccess {
+                setNotifyRecentOrderView(it)
+            }.onFailure {
+                Log.d(TAG, "getRecentOrder: 실패")
+            }
+        }
+    }
+
+    private fun setNotifyRecentOrderView(iceCreamOrderList: List<IceCreamOrder>) {
+        orderListAdapter.iceCreamOrderList = iceCreamOrderList
+        orderListAdapter.notifyDataSetChanged()
     }
 }
